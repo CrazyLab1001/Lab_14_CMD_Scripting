a28b13f1197dfd54aed9462990376d32f8c9b4b4
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-04-29 09:24:23 -0400

    Fourth Commit; Now unable to work WITH JFileChooser

--- src/FileScan.java
+++ src/FileScan.java
@@ -3,30 +3,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class FileScan {
     public static void main(String[] args) {
-        JFileChooser chooser = new JFileChooser();
+        // JFileChooser chooser = new JFileChooser();
         Scanner inFile;
         File selectedFile;
         String line;
-        String fileName;
+        // String fileName;
         int lineCount = 0;
         int wordCount = 0;
         int characterCount = 0;
         // ArrayList<Object> wordCounter = new ArrayList<>();
-        Path target = new File(System.getProperty("user.dir")).toPath();
-        target = target.resolve("src"); // opens in project src folder
-        chooser.setCurrentDirectory(target.toFile());
+        //Path target = new File(System.getProperty("user.dir")).toPath();
+        //target = target.resolve("src"); // opens in project src folder
+        Path target;
+        // chooser.setCurrentDirectory(target.toFile());
+        if (args.length > 0) {
+            target = Paths.get(args[0]);
+        } else {
+            JFileChooser chooser = new JFileChooser();
+            target = new File(System.getProperty("user.dir")).toPath();
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                target = chooser.getSelectedFile().toPath();
+            }
+            else {
+                System.out.println("You have to select a file! Ending program.");
+                System.exit(0);
+            }
+        }
         try  // Below is if file is selected
         {
-            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) // File choosen
-            {
-                selectedFile = chooser.getSelectedFile();
-                fileName = selectedFile.getName();
-                target = chooser.getSelectedFile().toPath();  // File object, not a String filename
-                inFile = new Scanner(target); // uses the scanner to analyze each line!
+            inFile = new Scanner(target);
+
                 do {
                     line = inFile.nextLine();
                     wordCount += line.split("\\s+").length;
@@ -36,17 +47,12 @@ public class FileScan {
                 } while (inFile.hasNextLine());
                 inFile.close(); // close file
                 System.out.println();
-                System.out.println("File name: " + fileName);
+                System.out.println("File name: " + target.toFile().getName());
                 System.out.println("Line Count: " + lineCount);
                 System.out.println("Word Count: " + wordCount);
                 System.out.println("Character Count: " + characterCount);
             }
-            else   // File chooser closed
-            {
-                System.out.println("You have to select a file! Ending program.");
-                System.exit(0);
-            }
-        }
+
         catch (FileNotFoundException e) // Errors other than unpicked file
         {
             System.out.println("File Not Found Error!");
